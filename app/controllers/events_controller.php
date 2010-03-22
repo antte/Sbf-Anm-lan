3e564a69a2d187266b818ef1829faa78759f7107
Olle Svensson <bolliolle@gmail.com>
2010-03-22 16:19:36 +0100

    person model

--- app/controllers/events_controller.php
+++ app/controllers/events_controller.php
@@ -11,5 +11,6 @@
 			if (!isset($eventId)) $this->redirect(array('action' => 'index'));
 			if (is_numeric($eventId)) $this->set('event', $this->Event->findById($eventId));
 			if (is_string($eventId)) $this->set('event', $this->Event->findByName($eventId));
+	
 		}
 	}
