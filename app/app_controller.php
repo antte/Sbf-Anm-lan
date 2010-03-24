7a8bab44854a6a43086d142ca92f92660ba7480d
antte <andreas.fliesberg@gmail.com>
2010-03-24 14:52:18 +0100

    cosmetics

--- app/app_controller.php
+++ app/app_controller.php
@@ -3,10 +3,9 @@
 		//var $components = array('Security'); TODO kolla hur funkar
 		
 		/**
-		 * Push something into an array in session (couldn't find this functionality anywhere)
-		 * please improve this by using an existing function ;)
-		 * @param string $arrayName
-		 * @param mixed $dataToPush
+		 * Takes data from controllers and puts in in the sesssion so that we can save it later
+		 * @param string $modelName to which name would you like to push (name should be the same as the model that will later be used to save the data)
+		 * @param mixed $dataToPush this->data from the different controllers that contains the information that we'll later save
 		 */
 		function saveModelDataToSession($modelName, $dataToPush) {
 			// Kan vara ide att hämta från någon konfigfil
@@ -14,6 +13,18 @@
 			$array = $this->Session->read($arrayName);
 			$array[$modelName] = $dataToPush[$modelName];
 			$this->Session->write($arrayName, $array);
+		}
+		
+		/**
+		 * Push something into an array in session (couldn't find this functionality anywhere)
+		 * please improve this by using an existing function ;)
+		 * @param string $arrayName
+		 * @param mixed $dataToPush
+		 */
+		function pushToSessionArray($arrayName, $dataToPush) {
+			$array = $this->Session->read($arrayName);
+			$array[] = $dataToPush;
+			$this->Session-write($arrayName, $array);
 		}
 		
 	}
