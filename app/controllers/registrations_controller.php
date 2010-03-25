afc2b1f3804d1c3b8e5a086e9f45de0880019667
antte <andreas.fliesberg@gmail.com>
2010-03-25 15:30:08 +0100

    Working on sending email to the user

--- app/controllers/registrations_controller.php
+++ app/controllers/registrations_controller.php
@@ -2,13 +2,28 @@
 
 class RegistrationsController extends AppController {
 	
+	var $components = array('Email');
+	
 	/**
 	 * Finalizes the registration (saving it)
 	 */
 	function finalize() {
+		
+		// The only thing registration needs right now is an event id
 		$eventId['Registration']['event_id'] = $this->Session->read('eventId');
 		$this->saveModelDataToSession('Registration', $eventId);
-		$saveStatus = $this->Registration->saveAndReturnStatus($this->Session->read('Registration'));
+		
+		$registration = $this->Session->read('Registration');
+		
+		if(!$this->Registration->saveAll($registration)) {
+			$this->Session->setFlash('Vi ber om ursäkt, din registrering kunde inte slutföras. Kontakta support.');
+			$this->redirect(array('controller' => 'events', 'action' => 'index'));
+		}
+		
+		$this->Email->from		= 'Svenska bilsportförbundet <info@sbf.se>';
+		$this->Email->to		= "{$registration['Registrator']['first_name']} {$registration['Registrator']['last_name']} <{$registration['Registrator']['email']}>";
+		$eventName = $this->Registration->Event->findById($eventId['Registration']['event_id'], array('fields' => 'name'));
+		$this->Email->subject	= "Kvitto för din anmälan till $eventName";
 	}
 	
 	function clearSession() {
---
+++ app/views/layouts/email/html/default.ctp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	
+	</head>
+	<body>
+		<?php echo $content_for_layout; ?>
+	</body>
+</html>
