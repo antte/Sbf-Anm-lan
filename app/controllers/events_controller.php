f0019ec0657e90295209e45fd4a5db5eeaf9572a
Olle Svensson <bolliolle@gmail.com>
2010-03-29 13:28:27 +0200

    kommenterat people och event controller

--- app/controllers/events_controller.php
+++ app/controllers/events_controller.php
@@ -3,16 +3,27 @@
 		
 		var $helpers = array('Html', 'Form', 'Javascript');
 		
+		/**
+		 * Send array of all events in the database to the index view
+		 */
 		function index() {
 			$this->set('events', $this->Event->find('all'));	
 		}
 		
+		/**
+		 * Get the event id and send event info to the view
+		 * @param unknown_type $eventId
+		 */
 		function view($eventId = null) {
 			if (!isset($eventId)) $this->redirect(array('action' => 'index'));
 			if (is_numeric($eventId)) $this->set('event', $this->Event->findById($eventId));
 			if (is_string($eventId)) $this->set('event', $this->Event->findByName($eventId));
 		}
 		
+		/**
+		 * Save the selected event in session and redirekt to next step
+		 * @param $eventId
+		 */
 		function next($eventId) {
 			$this->Session->write('Registration.Registration.event_id', $eventId);
 			$this->redirect(array('controller' => 'people', 'action' => 'create'));
