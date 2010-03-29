ee47ccba16a32485d2999faf306752a49d02b6a2
Olle Svensson <bolliolle@gmail.com>
2010-03-29 10:32:06 +0200

    reciept in progress

--- app/controllers/registrations_controller.php
+++ app/controllers/registrations_controller.php
@@ -10,47 +10,50 @@ class RegistrationsController extends AppController {
 	function finalize() {
 		
 		// The only thing registration needs right now is an event id
-		$Registration['Registration']['event_id'] = $this->Session->read('eventId');
+		$Registration['Registration']['event_id'] = $this->Session->read('Registration.Registration.event_id');
 		$kolla = $Registration['Registration']['number'] = $this->Registration->generateUniqueNumber();
 		$this->saveModelDataToSession('Registration', $Registration);		
 		$registration = $this->Session->read('Registration');
-		
+		//debug($registration);
 		if(!$this->Registration->saveAll($registration)) {
 			$this->Session->del('Registration');
 			$this->Session->setFlash('Vi ber om ursäkt, din registrering kunde inte slutföras. Kontakta support.');
 			$this->redirect(array('controller' => 'events', 'action' => 'index'));
+		} else {
+		
+			$this->Email->smtpOptions = array(
+				'port'			=> '25', 
+				'timeout'		=> '30',
+				'host' 			=> 'localhost'
+			);
+			
+			$this->Email->delivery 	= 'smtp';
+			
+			$this->Email->from		= 'Svenska bilsportförbundet Anmälan <anmalan@sbf.se>';
+			$this->Email->to		= "{$registration['Registrator']['first_name']} {$registration['Registrator']['last_name']} <{$registration['Registrator']['email']}>";
+			
+			$eventName = $this->Registration->Event->findById($registration['Registration']['event_id'], array('fields' => 'name'));
+			
+			$this->Email->subject	= "Kvitto för din anmälan till $eventName";
+			$this->Email->template	= 'receipt';
+			$this->Email->sendAs	= 'both'; //both text and html
+			$this->set('Registration', $registration);
+			//Save boockingnumber delete everything else 
+			$this->Session->write('booking_number',$registration['Registration']['number']);
+			$this->Session->del('Registration');
+			$this->Email->send();
+			$this->redirect(array ('action' => 'receipt'));
 		}
-		
-		$this->Email->smtpOptions = array(
-			'port'			=> '25', 
-			'timeout'		=> '30',
-			'host' 			=> 'localhost'
-		);
-		
-		$this->Email->delivery 	= 'smtp';
-		
-		$this->Email->from		= 'Svenska bilsportförbundet Anmälan <anmalan@sbf.se>';
-		$this->Email->to		= "{$registration['Registrator']['first_name']} {$registration['Registrator']['last_name']} <{$registration['Registrator']['email']}>";
-		
-		$eventName = $this->Registration->Event->findById($eventId['Registration']['event_id'], array('fields' => 'name'));
-		
-		$this->Email->subject	= "Kvitto för din anmälan till $eventName";
-		$this->Email->template	= 'receipt';
-		$this->Email->sendAs	= 'both'; //both text and html
-		$this->set('registration', $registration);
-		$this->Session->del('Registration');
-		$this->Email->send();
-		debug($this->Email->smtpError);
 	}
 	
 	function clearSession() {
 		$this->Session->del('Registration');
 		$this->Session->setFlash('Session rensad');
-		$this->redirect(array ('action' => 'create'));
+		$this->redirect(array ('action' => 'create', 'controller' =>'registrator'));
 	}
 	
 	function receipt() {
-		$registration = $this->Session->Read('Registration.Registration');
+		$registration = $this->Session->read('booking_number');
 		$registration['event_name'] = $this->Registration->Event->field('name',array('id'=> $registration['event_id'] ));
 		//debug($registration);
 		return $registration;
