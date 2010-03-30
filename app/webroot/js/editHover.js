1f286486115d5d6050a525e6465ffc285203ed5f
Markus Nordin <markus.nordin@gmail.com>
2010-03-30 12:49:00 +0200

    Goldplating the review module

--- app/webroot/js/editHover.js
+++ app/webroot/js/editHover.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 	$('.edit_link a').hover(
 			  function () {
 				    $(this).parents('div:first').addClass("hover");
-				    //$(this).parents('div').sibling
+				    $(this).parents('div:first').prevAll('div:first').addClass("hover");
 				    
 				  },
 				  function () {
-				    //$(this).removeClass("hover");
 				    $(this).parents('div:first').removeClass("hover");
+				    $(this).parents('div:first').prevAll('div:first').removeClass("hover");
 				  }
 				);
 });
