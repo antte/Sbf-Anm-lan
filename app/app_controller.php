ff3c0e567592164b5b0cdd696dfbc67c3dcdaa58
antte <andreas.fliesberg@gmail.com>
2010-04-09 12:52:35 +0200

    If you ask a controller very nicely to finalizeStep it might just advance one step and save model data to session

--- app/app_controller.php
+++ app/app_controller.php
@@ -1,20 +1,45 @@
 <?php
 
 	App::import('Sanitize');
-	
+	App::import('Inflector');
+		
 	class AppController extends Controller {
 		
 		/**
-		 * Takes data from controllers and puts in in the sesssion so that we can save it later
-		 * @param string $modelName to which name would you like to push (name should be the same as the model that will later be used to save the data)
-		 * @param mixed $dataToPush this->data from the different controllers that contains the information that we'll later save
+		 * This function will blow up if the $controller doesn't have any data to save (it likes to blow up in more cases)
+		 * @param $controller reference to the calling controller ($this from the controllers perspective)
 		 */
-		function saveModelDataToSession($modelName, $dataToPush) {
-			// Kan vara ide att hämta från någon konfigfil
+		function finalizeStep(&$currentController) {
+			
+			if(!$this->requestAction('steps/advanceOneStep')){
+				throw new Exception('Can\'t advance one step');
+			}
+				
+			//saves the data to session
+			try{
+				$this->saveModelDataToSession($currentController);
+			} catch (Exception $e) {
+				throw($e);
+			}
+			return true;
+		}
+	
+	/**
+	 * Takes data from controllers and puts in in the sesssion so that we can save it later
+	 * @param string $modelName to which name would you like to push (name should be the same as the model that will later be used to save the data)
+	 * @param mixed $dataToPush this->data from the different controllers that contains the information that we'll later save
+	 */
+	private function saveModelDataToSession(&$currentController) {
+		try {
 			$arrayName = 'Registration';
 			$array = $this->Session->read($arrayName);
-			$array[$modelName] = $dataToPush[$modelName];
+			$modelName = ucfirst(Inflector::singularize($currentController->params['controller']));
+			$array[$modelName] = $currentController->data[$modelName];
 			$this->Session->write($arrayName, $array);
+		} catch(Exception $e) {
+			return $e;
 		}
-		
+		return true;
 	}
+		
+}
--- app/controllers/steps_controller.php
+++ app/controllers/steps_controller.php
@@ -74,5 +74,35 @@ class StepsController extends AppController {
 		//return $this->Event->Step->findById($eventId);
 
 	}
-
+	
+	/**
+	 * sets current to previous and next to current
+	 */
+	function advanceOneStep() {
+		
+		//we don't allow this action to be used unless requested
+		if(!isset($this->params['requested'])) { return; }
+		
+		$steps = $this->Session->read('Steps');
+		
+		//change that step arrays state value to previous
+		$currentFound = false;
+		$i = 0;
+		foreach($steps as &$step) { 
+			if ($step['state'] == 'current') {
+					$step['state'] = 'previous';
+					$currentFound = true;
+			} else if ($currentFound) {
+				//sets the next step in the rocket as current
+				$step['state'] = 'current';
+				break;
+			}
+			$i++;
+		}
+		
+		// after we change steps we need to write it to session or nothing will happen 
+		// & we lets requester know whether it fails or succeeds
+		return $this->Session->write('Steps', $steps);
+	}
+	
 }
