3cafbb4a7e535d4287c2f9401dce8b8ae38a5591
antte <andreas.fliesberg@gmail.com>
2010-04-22 15:56:02 +0200

    new admin_label

---
+++ notes/steps_data.sql
@@ -0,0 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.0.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 22, 2010 at 01:53 PM
+-- Server version: 5.1.36
+-- PHP Version: 5.3.0
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `sbf-anmalan`
+--
+
+--
+-- Dumping data for table `steps`
+--
+
+INSERT INTO `steps` (`id`, `name`, `admin_label`, `controller`, `action`) VALUES
+(1, 'Sällskap', 'Anmälda', 'People', 'create'),
+(2, 'Kontaktuppgifter', 'Bokningar', 'Registrators', 'create'),
+(3, 'Granska', '', 'Registrations', 'review'),
+(4, 'Kvitto', '', 'Registrations', 'receipt');
